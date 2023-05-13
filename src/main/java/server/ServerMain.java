17f312e49fe8beac87f120cc472b14a1f8ee9a2a
chengjiahui <chengjiahui@baidu.com>
2023-05-13 19:54:21 +0800

    first commit

---
+++ src/main/java/server/ServerMain.java
@@ -0,0 +1,12 @@
+package server;
+
+/**
+ * @Auther: chengjiahui
+ * @Date: 2023/5/13
+ * @Description:
+ */
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("hello http web");
+    }
+}
